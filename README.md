1f4c29ce03c1202e1cf5577970a2c97b262e252e
egon <121918108+EgonLh@users.noreply.github.com>
2024-12-05 11:00:35 +0630

    add ch-1,2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Learning_py
 Notes For Learning Python
+
+> Python Crash Courese  
+
+- Chapter `1`  :  [x]
+- Chapter `2`  :  [x]
--- index.py
+++ index.py
@@ -1,2 +1,41 @@
 print("Hello , Python!");
 
+# Chatper 2  : Variable and Simple Data Types
+
+# Changing Cases
+_str = "egon lothbork"
+
+print("title() :" + _str.title()); # don't mutate , the orginial one - create a new one
+print("lower() :"+_str.lower());
+print("upper() :" +_str.upper());
+print("normal :" +_str);
+
+
+#Combining or Concatenating String
+print("\nCombining String\n")
+f_name = "egon"
+l_name = "loth"
+
+name = f_name + " " + l_name;
+print("fname + ' ' + lname :" +name)
+
+
+#stripping Whitespace
+
+str_white =  "Hello , World      "
+print(str_white.rstrip() + " : Without Whitespace");
+print(str_white + " : with Whitespace")
+
+str_0 = " Hello, World  ";
+print(":"+str_0+":")
+print(":"+str_0.lstrip()+":")
+print(":"+str_0.rstrip()+":")
+print(":"+str_0.strip()+":")
+
+
+# Avoiding Type Errors with the str() Function
+age =  2;
+print("Hello, your age is " + str(age))
+
+
+# Follow the guideline of The Zen of Python by Tim Peters
