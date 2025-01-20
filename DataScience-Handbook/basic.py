a8c051d20c7f57a08617843f8eb368f1c7fc0ec3
egon <121918108+EgonLh@users.noreply.github.com>
2025-01-20 09:55:49 +0630

    add-file

---
+++ DataScience-Handbook/basic.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+def add(a,b):
+    return a+b;
+    
+get_ipython().run_line_magic('pinfo2', 'add')
+get_ipython().run_line_magic('pinfo', 'add')
+get_ipython().run_line_magic('cls', '')
+get_ipython().run_line_magic('pinfo', 'len')
+get_ipython().run_line_magic('pinfo2', 'len')
+get_ipython().run_line_magic('save', '')
+get_ipython().run_line_magic('save', '')
+get_ipython().run_line_magic('save', 'basic')
+get_ipython().run_line_magic('save', 'basic.py')
+get_ipython().run_line_magic('cls', '')
+a = {"one":1}
+first class function
+get_ipython().run_line_magic('save', '')
+get_ipython().run_line_magic('save', 'basic.py')
+get_ipython().run_line_magic('cls', '')
+a = {"one":1}
+get_ipython().run_line_magic('pinfo2', 'a')
+get_ipython().run_line_magic('pinfo', 'a')
+def showlog(a):
+    print("Log :"1);
+def showlog(a):
+    print("Log :",a);
+    
+get_ipython().run_line_magic('pinfo', 'showlog')
+get_ipython().run_line_magic('pinfo2', 'showlog')
+get_ipython().run_line_magic('cls', '')
+def showlogWithDocString(a):
+    """Showing the log"""
+    print("Log :",a);
+    
+get_ipython().run_line_magic('pinfo2', 'showlogWithDocString')
+get_ipython().run_line_magic('pinfo', 'showlogWithDocString')
+get_ipython().run_line_magic('cls', '')
+get_ipython().run_line_magic('save', 'basic.py')
+get_ipython().run_line_magic('cls', '')
+get_ipython().run_line_magic('pinfo', 'a')
+get_ipython().run_line_magic('psearch', '*Warning')
+get_ipython().run_line_magic('psearch', '*Error')
+str = "Hello World"
+get_ipython().run_line_magic('psearch', 'str.*find*')
+atype of flexible wildcard search can be useful for finding a particular command when getting 
+home
+home
+get_ipython().run_line_magic('history', '')
